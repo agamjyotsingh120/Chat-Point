2248755e91bf94b46947ab716040979805589097
Agamjyot Singh <80823915+agamjyotsingh120@users.noreply.github.com>
2021-12-21 10:23:31 +0530

    Add files via upload

--- chatr.js
+++ chatr.js
@@ -12,10 +12,30 @@ var firebaseConfig = {
   firebase.initializeApp(firebaseConfig);
 //ADD YOUR FIREBASE LINKS HERE
 
-function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
-     Room_names = childKey;
-    //Start code
+user_name = localStorage.getItem("user_name");
+document.getElementById("user_name").innerHTML = "Welcome " + user_name + " !";
 
-    //End code
-    });});}
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+  Room_names = childKey;
+ //Start code
+
+ console.log("room Name = " + Room_names);
+ Row = "<div class='room_name' id=" +Room_names + " onclick='redirect(this.id)'  >#" + Room_names +"</div><hr>";
+ document.getElementById("output").innerHTML += row;
+ //End code
+ });});}
 getData();
+
+function redirect()
+{
+ console.log(name);
+ localStorage.setItem("room_name", name);
+ window.location = "kwitter_page.html";
+}
+
+
+function logout(){
+  localStorage.removeItem("user_name");
+  localStorage.removeItem("room_name");
+  window.location = "index.html";
+}
